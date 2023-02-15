71cf8aef603f9eee43c611ab848392c0b7c9be9f
Bfrpaulon <brunopaulon@outlook.com.br>
2023-02-15 15:11:00 -0300

    update main.css and index.html

---
+++ Assets/js/main.js
@@ -0,0 +1,9 @@
+let nome = prompt("Qual é o seu nome?");
+
+function exibirAlerta() {
+    alert("Tudo certo! " + nome + ".\n Sua inscrição será efetivada em 2 dias após o pagamento do boleto.");
+}
+
+function exibirMensagem() {
+    alert(`Tudo certo! ${nome}. \n Sua Mensagem foi enviada com sucesso. Verique sempre sua Caixa de Email.`);
+}
