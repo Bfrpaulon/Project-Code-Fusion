e4ba0182454ea5f7fdfadb54f9de3a3217483e1d
Bfrpaulon <brunopaulon@outlook.com.br>
2023-02-22 20:50:43 +0000

    update

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,1 @@
-let nome = prompt("Qual é o seu nome?");
-while (nome === "") {
-    nome = prompt("Por favor, digite seu nome:");
-}
-function exibirAlerta() {
-    alert("Tudo certo! " + nome + ".\n Sua inscrição será efetivada em 2 dias após o pagamento do boleto.");
-}
-
-function exibirMensagem() {
-    alert(`Tudo certo! ${nome}. \n Sua Mensagem foi enviada com sucesso. Verique sempre sua Caixa de Email.`);
 }
